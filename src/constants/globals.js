ee0668c7801994bc89be49d8477e3c7dfa73d0f1
Beau Enslow <72932986+GravviSoft@users.noreply.github.com>
2025-03-27 12:08:56 -0600

    Update globals.js

--- src/constants/globals.js
+++ src/constants/globals.js
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://localhost'
-const baseUrl = 'https://440f142c.dv-mtn-capstone.pages.dev'
+const baseUrl = 'https://dv-mtn-capstone-ejqocnr57-beau-enslows-projects.vercel.app'
 const themeDefault = 'dark'
 const namesOfModes = ['dark', 'moonlight', 'eclipse', 'light']
 
